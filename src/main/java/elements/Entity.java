2250ef541ef615e8d11358f6b9e54f5c4fc79bba
Thiago Pereira <thiagopereiramail@gmail.com>
2021-11-13 19:20:52 -0300

    Criando a classe base para todas as entidades

---
+++ src/main/java/elements/Entity.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elements;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+/**
+ *
+ * @author thiago
+ */
+public class Entity {
+    public int x,y;
+    public int width, height;
+    private Image sprite;
+    
+    public Entity(int x, int y, Image sprite) {
+        this.x = x;
+        this.y = y;
+        this.width = 32;
+        this.height = 32;
+        this.sprite = sprite;
+    }
+    
+    public Entity(int x, int y, int w, int h, Image sprite) {
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
+        this.sprite = sprite;
+    }
+    
+    public void tick() {
+    }
+    
+    public void render(Graphics g) {
+        g.drawImage(sprite, x, y, width, height, null);
+    }
+}
