4baa616f7860a82c91dc995ea9c21360e7b5c7b1
Thiago Pereira <thiagopereiramail@gmail.com>
2021-11-15 15:23:42 -0300

    Método para ler arquivos.

--- src/main/java/elements/Funcs.java
+++ src/main/java/elements/Funcs.java
@@ -6,6 +6,7 @@
 package elements;
 
 import java.io.File;
+import java.util.Scanner;
 
 /**
  *
@@ -15,5 +16,27 @@ public class Funcs {
     public static File[] loadPath(String path) {
         File file = new File(path);
         return file.listFiles();
+    }
+    
+    public static String readFile(final String path) {
+        try {
+            StringBuilder data = new StringBuilder();
+            File file = new File(path);
+            
+            if (file.exists()) {
+                Scanner reader = new Scanner(file);
+                
+                if (reader.hasNext()) {
+                    data.append(reader.nextLine());
+                }
+                
+                reader.close();
+            }
+            
+            return data.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
